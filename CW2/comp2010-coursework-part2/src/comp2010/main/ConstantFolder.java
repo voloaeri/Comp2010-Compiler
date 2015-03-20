a62fe67dcf6e839098e0ee918f6f91584f50c735
Patrick Steffens <patricksteffens89@gmail.com>
2015-03-20 21:37:13 +0000

    Simple folding for given (simple) example

--- CW2/comp2010-coursework-part2/src/comp2010/main/ConstantFolder.java
+++ CW2/comp2010-coursework-part2/src/comp2010/main/ConstantFolder.java
@@ -3,9 +3,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+import java.util.ArrayList;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Arrays;
 
 import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.JavaClass;
@@ -24,6 +25,7 @@ import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.ArithmeticInstruction;
 import org.apache.bcel.generic.MethodGen;
 import org.apache.bcel.generic.Type;
+import org.apache.bcel.generic.TargetLostException;
 import org.apache.bcel.Constants;
 
 public class ConstantFolder
@@ -46,12 +48,15 @@ public class ConstantFolder
 		}
 	}
 	
-	enum OperationType {
+	enum OperationType 
+	{
 		ADD, SUB, DIV, MUL, NEG, REM, AND, OR, SHL, SHR, USHR, XOR, NONE
 	}
 	
-	private OperationType getType(Instruction instr) {
-		switch (instr.getOpcode()) {
+	private OperationType getOpType(Instruction instr) 
+	{
+		switch (instr.getOpcode()) 
+		{
 			case Constants.IADD: //Fall through
 			case Constants.LADD: //Fall through
 			case Constants.FADD: //Fall through
@@ -66,10 +71,44 @@ public class ConstantFolder
 		return OperationType.NONE;
 	}
 	
-	private void simpleFolding(ClassGen gen, ConstantPoolGen cpgen, Method method) {
+	private Object add(Type t, Object a, Object b) 
+	{
+		switch (t.getType()) 
+		{
+			case Constants.T_INT:
+				return new Integer(((Number)a).intValue()+((Number)b).intValue());
+			case Constants.T_LONG:
+				return new Long(((Number)a).longValue()+((Number)b).longValue());			
+			case Constants.T_FLOAT:
+				return new Float(((Number)a).floatValue()+((Number)b).floatValue());
+			case Constants.T_DOUBLE:
+				return new Double(((Number)a).doubleValue()+((Number)b).doubleValue());
+			default:
+				break;
+		};
+
+		return null;
+	}
+
+	private Object calc(ArithmeticInstruction instr, ConstantPoolGen cpgen, Object a, Object b) 
+	{
+		switch (getOpType(instr)) 
+		{
+			case ADD:
+				return this.add(instr.getType(cpgen), a,b);
+			default:
+				break;
+		};
+
+		return null;
+
+	}
+
+	private void simpleFolding(ClassGen gen, ConstantPoolGen cpgen, Method method) 
+	{
 		// Get the Code of the method, which is a collection of bytecode instructions
 		Code methodCode = method.getCode();
-		//System.out.println(methodCode.toString());
+		//System.out.println(methodCode);
 		InstructionList instList = new InstructionList(methodCode.getCode());
 
 		// Initialise a method generator with the original method as the baseline	
@@ -79,24 +118,80 @@ public class ConstantFolder
 		ConstantPool cp = cpgen.getConstantPool();
 		// get the constants in the pool
 		
-		Constant[] constants = cp.getConstantPool();
-		Deque<Object> stack = new ArrayDeque<Object>();
+		Deque<Object> constantStack = new ArrayDeque<Object>();
+		ArrayList<InstructionHandle> removeHandles = new ArrayList<InstructionHandle>();
+		boolean remove = false;
 		for (InstructionHandle handle : instList.getInstructionHandles()) 
 		{
 			Instruction instr = handle.getInstruction();
 
-			if (instr instanceof LDC) {
+			if (instr instanceof LDC) 
+			{
 				LDC ldc = (LDC) instr;
-				stack.addFirst(ldc.getValue(cpgen));
-				System.out.println(ldc.getOpcode());
-			}
-			if (instr instanceof ArithmeticInstruction) {
-				
-				System.out.println(instr.toString());
+				remove = true; // start adding all following instructions to remove list
+				removeHandles.add(handle);
+				constantStack.addFirst(ldc.getValue(cpgen));
+			} 
+			else if (instr instanceof ArithmeticInstruction) 
+			{
+				remove = false; // Found an operation ==> stop removing
+
 				ArithmeticInstruction arith = (ArithmeticInstruction) instr;
-				System.out.println(arith.getType(cpgen));
-				System.out.println(getType(arith));
-				System.out.println(arith.produceStack(cpgen));
+
+				// Get last two loaded constants from constantStack
+				Object a = constantStack.pop();
+				Object b = constantStack.pop();
+				
+				// Perform calculation
+				Object result = calc(arith, cpgen, a, b);
+				
+				int index = -1;
+				switch(arith.getType(cpgen).getType()) 
+				{
+					case Constants.T_INT:
+						index = cpgen.addInteger(((Integer)result).intValue());
+						break;
+					case Constants.T_LONG:
+						index = cpgen.addLong(((Long)result).longValue());			
+						break;
+					case Constants.T_FLOAT:
+						index = cpgen.addFloat(((Float)result).floatValue());
+						break;
+					case Constants.T_DOUBLE:
+						index = cpgen.addDouble(((Double)result).doubleValue());
+						break;
+					default:
+						break;
+				};
+
+				instList.insert(handle, new LDC(index));
+				
+				//System.out.println(result);
+
+				//System.out.println(arith.getType(cpgen));
+				//System.out.println(getType(arith));
+			} 
+			else 
+			{
+				if (remove) 
+					removeHandles.add(handle);
+			}
+		}
+
+		// Remove deprecated instructions
+		for (InstructionHandle h: removeHandles) 
+		{
+			// delete the old one
+			try
+			{
+				// delete the old one
+				instList.delete(h);
+
+			}
+			catch (TargetLostException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 
@@ -112,13 +207,17 @@ public class ConstantFolder
 		Method newMethod = methodGen.getMethod();
 		// replace the method in the original class
 		gen.replaceMethod(method, newMethod);
-	}
-
-	private void constantFolding(ClassGen gen, ConstantPoolGen cpgen, Method method) {
+		//System.out.println(newMethod.getCode());
 
 	}
 
-	private void dynamicFolding(ClassGen gen, ConstantPoolGen cpgen, Method method) {
+	private void constantFolding(ClassGen gen, ConstantPoolGen cpgen, Method method) 
+	{
+
+	}
+
+	private void dynamicFolding(ClassGen gen, ConstantPoolGen cpgen, Method method) 
+	{
 
 	}
 
@@ -148,33 +247,7 @@ public class ConstantFolder
 		Method[] methods = gen.getMethods();
 		for (Method m : methods)
 		{
-
 			optimizeMethod(gen, cpgen, m);
-
-		}
-
-
-
-
-		for (int i = 0; i < constants.length; i++)
-		{
-			// string constants take two entries in the pool
-			// the first one is of ConstantString, which contains
-			// an index to the second entry, which is ConstantUtf8
-			// (displayed Asciz when disassembled by javap)
-			//
-			// ConstantUtf8 (Asciz) entries are used to store method names, etc
-			// whereas we are only interested in String constants
-			// So we first look for ConstantString entry, 
-			// then retrieve the index of ConstantUtf8 entry, which we then replace
-			
-
-			if (constants[i] instanceof ConstantString)
-			{
-				ConstantString cs = (ConstantString) constants[i];
-				cp.setConstant(cs.getStringIndex(), 
-								new ConstantUtf8("Bwahaha!"));
-			}
 		}
 
 		// Do your optimization here
@@ -184,7 +257,6 @@ public class ConstantFolder
 	public void write(String optimisedFilePath)
 	{
 		this.optimize();
-
 		try {
 			FileOutputStream out = new FileOutputStream(new File(optimisedFilePath));
 			this.optimized.dump(out);
